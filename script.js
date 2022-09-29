8449a8c9d90361cb159851f4f14efb31497ba59a
Marco Tortolani <99374393+marcotortolani@users.noreply.github.com>
2022-09-28 21:42:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const body = document.querySelector("body");
+
+const rings = document.querySelector(".rings");
+
+if (body.className === "inicio") {
+    
+    
+
+    // setInterval(() => {
+    //     rings.classList.toggle("scale");
+
+    // }, 2000);
+}
+
+if (body.className === "date") {
+
+    // setInterval(() => {
+    //     rings.classList.toggle("spin");
+    // }, 2000);
+
+}
